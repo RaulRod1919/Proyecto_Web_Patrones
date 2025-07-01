58ecf082d7a30969de3879f887a125f5388b89f8
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-06-30 19:58:28 -0600

    Proyecto de GRS CONSTRUCCIONES

---
+++ src/main/java/com/GRS/GrsApplication.java
@@ -0,0 +1,13 @@
+package com.GRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GRS/GrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
