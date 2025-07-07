724892a91193736e23cecafe484a709a3e95d92a
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-07-06 19:38:47 -0600

    Arreglo del index

--- src/main/java/com/GRS/controller/IndexController.java
+++ src/main/java/com/GRS/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.GRS.controller;
 
+import com.GRS.services.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    
+    @Autowired
+    private EmpresaService empresaService;
 
     // Página principal
     @GetMapping({"", "/", "/index"})
     public String index(Model model) {
+        model.addAttribute("empresa", empresaService.getEmpresa());
         return "index"; // debe estar en: src/main/resources/templates/index.html
     }
 
